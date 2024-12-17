7bc8471b5b451c631c47136f4226f2cd4c3ab14c
Melissa Panduro <127220575+MelissaPanduro@users.noreply.github.com>
2024-12-17 16:11:49 +0000

    ..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apigpt_fe: 'https://miniature-bassoon-g4xpvvxpjqghwrvj-8085.app.github.dev/api'
-  apigpt_fe: `${API_BACKEND}`
+  apigpt_fe: 'https://ominous-memory-v4xxwvxxjq5269pg-8085.app.github.dev/api'
+  //apigpt_fe: `${API_BACKEND}`
 };
