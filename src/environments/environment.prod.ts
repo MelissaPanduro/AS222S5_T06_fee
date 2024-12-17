f3740ec491b3143bc378b020dfd9d18bff8a186f
Melissa Panduro <127220575+MelissaPanduro@users.noreply.github.com>
2024-12-17 16:38:21 +0000

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apigpt_fe: 'https://miniature-bassoon-g4xpvvxpjqghwrvj-8085.app.github.dev/api'
+  apigpt_fe: 'https://urban-space-zebra-qrww6qwwxxwc99rr-8085.app.github.dev/api'
   //apigpt_fe: `${API_BACKEND}`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apigpt_fe: 'https://miniature-bassoon-g4xpvvxpjqghwrvj-8085.app.github.dev/api'
+  apigpt_fe: 'https://urban-space-zebra-qrww6qwwxxwc99rr-8085.app.github.dev/api'
   //apigpt_fe: `${API_BACKEND}`
 };
 
